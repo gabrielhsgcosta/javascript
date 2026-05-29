b5b07b2e92c5f4eb3bad3e8d68be2f3d54586b77
Gabriel Henrique <gabrielhenriquesilvagcosta@gmail.com>
2026-05-29 13:16:25 -0300

    Update ex017

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,16 +1,16 @@
 function contar() {
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert('[ERRO] Está faltando valores!')
     } else {
         res.innerHTML = 'Contando: <br>'
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
         if (p == 0) {
             alert('Impossível contar! CONSIDERANDO PASSO 1.')
             p = 1
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,19 +1,19 @@
-function tabuada(){
-    let num = document.getElementById('txtn')
+function calcular() {
+    let num = document.getElementById('txtnum')
     let tab = document.getElementById('seltab')
+
     if (num.value.length == 0) {
-        alert('[ERRO] Digite um número')
+        alert('[ERRO] Digite um número!')
     } else {
         let n = Number(num.value)
         let c = 1
-        tab.innerHTML = ''
-        while (c <= 10) {
+        tab.innerHTML = ``
+
+        for (c; c <= 10; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            item.valuec= `tab${c}`
+            item.setAttribute(`value`, `valor${c}`)
             tab.appendChild(item)
-            c++
         }
     }
-
 }
