064f49e8370c47e36036ac055e1e0d6e51a6ad89
Gabriel Henrique <gabrielhenriquesilvagcosta@gmail.com>
2026-05-29 12:21:14 -0300

    Update ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,22 +1,22 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById ('txtp')
-    let res = document.getElementById('res')
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados!')
+        alert('[ERRO] Está faltando valores!')
     } else {
         res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
         if (p == 0) {
             alert('Impossível contar! CONSIDERANDO PASSO 1.')
             p = 1
         }
-        
-        if (i < f){
+
+         if (i < f){
             // Contagem Crescente
             for (let c = i; c <= f; c += p) {
                 res.innerHTML += ` ${c} \u{1F449}`
